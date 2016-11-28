dcba3f39e17762c96139aaa673868705b8e4ac87
Jakub Chronowski <jakub@chronow.ski>
2016-11-28 18:25:27 +0700

    Disable focus on click for a more desktop feel.

--- app/index.js
+++ app/index.js
@@ -4,12 +4,15 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
+import { FocusStyleManager } from "@blueprintjs/core";
 import routes from './routes';
 import configureStore from './store/configureStore';
 import './app.global.scss';
 
 const store = configureStore();
 const history = syncHistoryWithStore(hashHistory, store);
+
+FocusStyleManager.onlyShowFocusOnTabs();
 
 render(
   <Provider store={store}>
