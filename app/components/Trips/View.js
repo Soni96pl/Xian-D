6972ea1816030af126ae76ea5a0e2cbb9be3fd04
Jakub Chronowski <jakub@chronow.ski>
2016-12-08 10:46:33 +0700

    Remove unused editClick function.

--- app/components/Trips/View.js
+++ app/components/Trips/View.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { tripsShape } from '../../shapes/trips';
 import Status from '../../components/Status';
 
-export default class Show extends Component {
+export default class View extends Component {
   static propTypes = {
     params: PropTypes.shape({
       tripId: PropTypes.string
@@ -14,16 +14,13 @@ export default class Show extends Component {
     router: React.PropTypes.object.isRequired
   };
 
-  editClick() {
-    const { tripId } = this.props.params;
-    this.context.router.push(`/trips/${tripId}/edit`);
-  }
-
   render() {
     const { tripId } = this.props.params;
     const trip = this.props.trips.filter((t) => t.id === parseInt(tripId, 10))[0];
     return (
-      <Status title={trip.name} />
+      <div>
+        <Status title={trip.name} />
+      </div>
     );
   }
 }
