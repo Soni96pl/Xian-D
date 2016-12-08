6543c893417a0ee5f6e71ce905b3b19ccdb73026
Jakub Chronowski <jakub@chronow.ski>
2016-12-08 11:00:28 +0700

    Make TripsView component 'dumb'.

--- app/components/Trips/View.js
+++ app/components/Trips/View.js
@@ -1,22 +1,14 @@
-import React, { Component, PropTypes } from 'react';
-import { tripsShape } from '../../shapes/trips';
+import React, { Component } from 'react';
+import { tripShape } from '../../shapes/trips';
 import Status from '../../components/Status';
 
 export default class View extends Component {
   static propTypes = {
-    params: PropTypes.shape({
-      tripId: PropTypes.string
-    }).isRequired,
-    trips: tripsShape.isRequired
+    trip: tripShape.isRequired
   }
 
-  static contextTypes = {
-    router: React.PropTypes.object.isRequired
-  };
-
   render() {
-    const { tripId } = this.props.params;
-    const trip = this.props.trips.filter((t) => t.id === parseInt(tripId, 10))[0];
+    const { trip } = this.props;
     return (
       <div>
         <Status title={trip.name} />
--- app/containers/Trips/ViewPage.js
+++ app/containers/Trips/ViewPage.js
@@ -1,5 +1,23 @@
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { tripsShape } from '../../shapes/trips';
 import View from '../../components/Trips/View';
+
+class ViewPage extends Component {
+  static propTypes = {
+    params: PropTypes.shape({
+      tripId: PropTypes.string
+    }).isRequired,
+    trips: tripsShape.isRequired
+  }
+
+  render() {
+    const { tripId } = this.props.params;
+    const trip = this.props.trips.filter((t) => t.id === parseInt(tripId, 10))[0];
+    return <View trip={trip} />;
+  }
+}
+
 
 function mapStateToProps(state) {
   return {
@@ -7,4 +25,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps, undefined)(View);
+export default connect(mapStateToProps, undefined)(ViewPage);
