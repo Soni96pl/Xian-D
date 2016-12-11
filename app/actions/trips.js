b8d7f6d2889a36d1662d84ae9811948e32936909
Jakub Chronowski <jakub@chronow.ski>
2016-12-11 14:34:34 +0700

    Add addTripTransport action to trips actions.

--- app/actions/trips.js
+++ app/actions/trips.js
@@ -2,6 +2,7 @@ import Barn from 'barn';
 import tripSchema from '../schema/trips';
 
 export const ADD_TRIP = 'ADD_TRIP';
+export const ADD_TRANSPORT = 'ADD_TRANSPORT';
 export const ADD_TRIP_TRANSPORT = 'ADD_TRIP_TRANSPORT';
 
 const barn = new Barn(localStorage);
@@ -16,6 +17,31 @@ export function addTrip({ name }) {
       id: tripId,
       transport: [],
       name
+    },
+    meta: {
+      schema: tripSchema
+    }
+  };
+}
+
+export function addTripTransport({ tripId, transport }) {
+  const transportId = barn.get('transportId') + 1;
+  barn.set('transportId', transportId);
+  return {
+    type: ADD_TRIP_TRANSPORT,
+    payload: {
+      id: tripId,
+      transport: [{
+        id: transportId,
+        carrier: transport.carrier || null,
+        code: transport.code || null,
+        mode: transport.mode || null,
+        departure: transport.departure || null,
+        arrival: transport.arrival || null,
+        price: transport.price || null,
+        booking: transport.booking || null,
+        conditions: transport.conditions || null
+      }]
     },
     meta: {
       schema: tripSchema
