73a16cdfa045fac2a631a21408ee343cb451e329
Jakub Chronowski <jakub@chronow.ski>
2016-12-14 15:18:38 +0700

    Make small TransportSegment component even more compact.

--- app/components/Transport/Segment.js
+++ app/components/Transport/Segment.js
@@ -1,29 +1,22 @@
 import React, { Component, PropTypes } from 'react';
-import { Button } from '@blueprintjs/core';
 import strftime from 'strftime';
 
 export default class Segment extends Component {
   static propTypes = {
     origin: PropTypes.string,
     destination: PropTypes.string,
-    departure: PropTypes.instanceOf(Date),
-    mode: PropTypes.string
+    departure: PropTypes.instanceOf(Date)
   }
 
   render() {
-    const { departure, origin, destination, mode } = this.props;
+    const { departure, origin, destination } = this.props;
     return (
       <div className="segment-small pt-card">
-        <div className="ticket-date">
-          <strong>{strftime('%A', departure)}</strong><br />
-          {strftime('%d %B %H:%M', departure)}
+        <div className="segment-date">
+          <strong>{strftime('%d %B %H:%M', departure)}</strong>
         </div>
         <div className="segment-places">
           {origin} <span className="pt-icon pt-icon-chevron-right" /> {destination}
-        </div>
-        <div className="segment-mode">{mode}</div>
-        <div className="segment-controls">
-          <Button text="Details" />
         </div>
       </div>
     );
--- app/components/Transport/Transport.js
+++ app/components/Transport/Transport.js
@@ -9,7 +9,6 @@ export default class Transport extends Component {
     const { children } = this.props;
     return (
       <div className="segments">
-        <h4><span className="pt-icon pt-icon-airplane"></span> Transport</h4>
         {children}
       </div>
     );
