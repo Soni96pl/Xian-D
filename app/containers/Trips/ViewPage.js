bd6caf7b5bf4379118e703c9a295a657aec58a48
Jakub Chronowski <jakub@chronow.ski>
2016-12-15 15:17:14 +0700

    Remove unused transportShape from ViewPage container.

--- app/containers/Trips/ViewPage.js
+++ app/containers/Trips/ViewPage.js
@@ -1,7 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { tripsShape } from '../../shapes/trips';
-import { transportShape } from '../../shapes/transport';
 import View from '../../components/Trips/ViewPage';
 
 class ViewPage extends Component {
