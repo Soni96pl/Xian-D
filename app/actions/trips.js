02d9dfee24f27df11bc25b642b5f6ca12655d7a7
Jakub Chronowski <jakub@chronow.ski>
2016-12-18 19:02:14 +0100

    Use normalizr to generate missing ids.

--- app/actions/trips.js
+++ app/actions/trips.js
@@ -1,20 +1,13 @@
-import Barn from 'barn';
 import tripSchema from '../schema/trips';
 
 export const ADD_TRIP = 'ADD_TRIP';
 export const ADD_TRANSPORT = 'ADD_TRANSPORT';
 export const ADD_TRIP_TRANSPORT = 'ADD_TRIP_TRANSPORT';
 
-const barn = new Barn(localStorage);
-if (barn.get('tripId') == null) barn.set('tripId', 0);
-
 export function addTrip({ name }) {
-  const tripId = barn.get('tripId') + 1;
-  barn.set('tripId', tripId);
   return {
     type: ADD_TRIP,
     payload: {
-      id: tripId,
       transport: [],
       name
     },
@@ -25,15 +18,11 @@ export function addTrip({ name }) {
 }
 
 export function addTripTransport({ tripId, transport }) {
-  const transportId = barn.get('transportId') + 1;
-  barn.set('transportId', transportId);
   return {
     type: ADD_TRIP_TRANSPORT,
     payload: {
       id: tripId,
-      transport: [
-        transport
-      ]
+      transport: [transport]
     },
     meta: {
       schema: tripSchema
