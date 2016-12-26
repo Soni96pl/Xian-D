da50de11ebdbc8b78e7871da7bba9813ae446272
Jakub Chronowski <jakub@chronow.ski>
2016-12-26 17:57:29 +0100

    Update tripShape to include segmentShape.

--- app/shapes/trips.js
+++ app/shapes/trips.js
@@ -1,9 +1,10 @@
 import { PropTypes } from 'react';
+import { segmentShape } from './transport';
 
 export const tripShape = PropTypes.shape({
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  transport: PropTypes.arrayOf(PropTypes.number).isRequired
+  transport: PropTypes.arrayOf(segmentShape).isRequired
 });
 
 export const tripsShape = PropTypes.objectOf(tripShape.isRequired);
