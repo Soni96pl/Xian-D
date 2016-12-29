39355d33315f2bdde920b3545709ed9df7fa7e35
Jakub Chronowski <jakub@chronow.ski>
2016-12-29 19:59:26 +0100

    Create CalendarCard component.

---
+++ app/components/CalendarCard.js
@@ -0,0 +1,23 @@
+import React, { Component, PropTypes } from 'react';
+import strftime from 'strftime';
+
+export default class CalendarCard extends Component {
+  static propTypes = {
+    date: PropTypes.instanceOf(Date)
+  }
+
+  render() {
+    const { date } = this.props;
+
+    return (
+      <div className="calendar-card pt-card pt-elevation-1">
+        <div className="calendar-card-month">
+          <h5>{strftime('%-d %b', date)}</h5>
+        </div>
+        <div className="calendar-card-day">
+          {strftime('%a', date)}
+        </div>
+      </div>
+    );
+  }
+}
